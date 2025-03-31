2906d93c33aa5df8e6d7f9240a1b10e7c36d0d91
retosugua <renanaugusto.ras@gmail.com>
2025-03-31 17:47:00 -0300

    sevice incompleto

--- first-app/src/app/home/home.component.ts
+++ first-app/src/app/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
 import { HousingLocationComponent } from '../housing-location/housing-location.component';
 import { HousingLocation } from '../housinglocation';
 import { CommonModule } from '@angular/common';
